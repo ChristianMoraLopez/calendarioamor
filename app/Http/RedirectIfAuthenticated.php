b546daa1fc4e1669a2c550defa7ad0cdb35640d5
Christian Rey Mora <christianmoralopez@hotmail.com>
2024-06-07 12:32:39 -0500

    cors

---
+++ app/Http/RedirectIfAuthenticated.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class RedirectIfAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if (Auth::guard($guard)->check()) {
+            // Si el usuario está autenticado, redirige a la página de inicio
+            return redirect('/home');
+        }
+
+        return $next($request);
+    }
+}
